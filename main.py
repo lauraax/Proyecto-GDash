b059cd3977d11ccedb1db7e1a73e5e7ff2ddb40e
lauraax <64974256+lauraax@users.noreply.github.com>
2024-05-20 07:48:10 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame 
+from pygame.locals import *
+from settings import *
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption("Puzzle Deslizante")
+
